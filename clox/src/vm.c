7ca768121773945dcc9f935afa784d5d0b912698
nsdeschenes <nsdeschenes@outlook.com>
2025-08-23 20:27:44 -0300

    complete chapter 17

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -86,7 +86,20 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    compile(source);
-    return INTERPRET_OK;
+InterpretResult interpret(const char* source) {
+    Chunk chunk;
+    initChunk(&chunk);
+
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+
+    InterpretResult result = run();
+
+    freeChunk(&chunk);
+    return result;
 }
